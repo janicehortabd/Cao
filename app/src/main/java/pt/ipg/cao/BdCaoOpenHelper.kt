932f480ae5d52d0a151543ce4cbc1f866fad3401
janicehortabd <janicebd2023@gmail.com>
2023-05-23 20:22:57 +0100

    apagar a base de dados antes de executar os testes

--- app/src/main/java/pt/ipg/cao/BdCaoOpenHelper.kt
+++ app/src/main/java/pt/ipg/cao/BdCaoOpenHelper.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-private const val NOME_BASE_DADOS = "cao.db"
+
 private const val VERSAO_BASE_DADOS = 1
 
 class BdCaoOpenHelper (
@@ -18,6 +18,9 @@ class BdCaoOpenHelper (
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
 
+    }
+    companion object {
+        const val NOME_BASE_DADOS = "cao.db"
     }
 }
 
