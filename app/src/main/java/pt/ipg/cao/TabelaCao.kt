67d0b3dfd73172b6eba68fdec2edf8c5b8755dc0
janicehortabd <janicebd2023@gmail.com>
2023-05-23 20:16:03 +0100

    erro ao criar tabela Cao

--- app/src/main/java/pt/ipg/cao/TabelaCao.kt
+++ app/src/main/java/pt/ipg/cao/TabelaCao.kt
@@ -6,7 +6,7 @@ import android.provider.BaseColumns
 
 class TabelaCao(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL), FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL, FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
