6bf3761b51e44a86d713e24dea92f8953c5e912a
Janice Horta <janicesanches2000@gmail.com>
2023-05-26 09:22:18 +0100

    criado adaptador dos livros(interface RecycleView)

---
+++ app/src/main/java/pt/ipg/cao/AdapterCao.kt
@@ -0,0 +1,12 @@
+package pt.ipg.cao
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterCao: RecyclerView.Adapter<AdapterCao.ViewHolderCao>() {
+    inner class ViewHolderCao(itemView: View) : ViewHolder(itemView){
+        
+    }
+
+}
