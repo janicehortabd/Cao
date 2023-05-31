4c98876fab3f6b133513bebc36c60102f6e2ce01
janicehortabd <janicebd2023@gmail.com>
2023-05-31 22:15:38 +0100

    Alterados identificadores da navegação e strings

---
+++ app/src/main/java/pt/ipg/cao/MenuPrincipalFragment.kt
@@ -0,0 +1,48 @@
+package pt.ipg.cao
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipg.cao.databinding.FragmentMenuPrincipalBinding
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MenuPrincipalFragment : Fragment() {
+
+    private var _binding: FragmentMenuPrincipalBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMenuPrincipalBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSobre.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.buttonCao.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_listaCaoFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
